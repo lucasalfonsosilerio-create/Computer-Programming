4410dba49e109fadb9b8d9296b51740b3e15fe5d
lucasalfonsosilerio-create <lucasalfonso.silerio@lhsla.org>
2026-08-29 13:55:43 -0700

    homework

---
+++ assignment1a.py
@@ -0,0 +1,9 @@
+# Silerio, Lucas
+# Computer Programming, P4
+# Assignment 1a
+# August 27 2026
+
+print("Hello World!\nHello! My name is Lucas!\nI love Python!\nI am 17 years old.")
+print(5+5, 4*3, 50-40)
+print(100/5,100//5) #Float for 100/5 versus Integer for 100//5
+print(2**4, 6*3-4+8//2)
