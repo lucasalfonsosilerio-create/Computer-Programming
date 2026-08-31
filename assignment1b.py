b0c7e06ff7f95a12c293601bceea34d6214b31b9
lucasalfonsosilerio-create <lucasalfonso.silerio@lhsla.org>
2026-08-31 09:02:31 -0700

    assignment 1b

---
+++ assignment1b.py
@@ -0,0 +1,9 @@
+fav_hobby = "Video Games"
+personal_message = "colin"
+famous_person = "Albert Einstein"
+famous_quote = "I have no special talent, I am only passionately curious."
+
+print("I love", fav_hobby.lower())
+print("Hello,", personal_message.title(), "I hope you are doing well.")
+print(personal_message.lower(),personal_message.upper(), personal_message.title())
+print(famous_person, "once said,", famous_quote)
