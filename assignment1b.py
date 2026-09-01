3aefe506d97664c86efee15175ceab1f92aaab71
lucasalfonsosilerio-create <lucasalfonso.silerio@lhsla.org>
2026-09-01 12:29:20 -0700

    Assignment 1B

--- assignment1b.py
+++ assignment1b.py
@@ -1,3 +1,8 @@
+# Silerio, Lucas
+# Computer Programming, Period 4
+# Assignment: Homework 2
+# 31 August 2026
+
 fav_hobby = "Video Games"
 personal_message = "colin"
 famous_person = "Albert Einstein"
@@ -6,4 +11,20 @@ famous_quote = "I have no special talent, I am only passionately curious."
 print("I love", fav_hobby.lower())
 print("Hello,", personal_message.title(), "I hope you are doing well.")
 print(personal_message.lower(),personal_message.upper(), personal_message.title())
-print(famous_person, "once said,", famous_quote)
+
+print(famous_person, "once said,", f'"{famous_quote}"')
+
+message = f'{famous_person} once said, "{famous_quote}"'
+
+print(message)
+
+name2 = " Emilia "
+print(f"{name2}\n{name2.lstrip()}\n{name2.rstrip()}\n{name2.strip()}")
+
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
+
+print(f"{11+0}\n{11-0}\n{11*1}\n{11/1}\n{11//1}")
+
+favnumber = 5
+print("My favorite number is", favnumber)
