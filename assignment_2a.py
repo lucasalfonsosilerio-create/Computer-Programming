95289bb42a907800997e29fcf1c4cc9ee367ce15
lucasalfonsosilerio-create <lucasalfonso.silerio@lhsla.org>
2026-09-15 09:40:53 -0700

    Assignment 2A

--- assignment_2a.py
+++ assignment_2a.py
@@ -16,4 +16,36 @@ list3 = list1 + list2
 print(list3)
 
 favFoods = ["french fries", "chicken nuggets", "burger", "fried rice", "ice cream"]
-print(len(favFoods), favFoods[-5], favFoods.append("Lucas"))
+print(len(favFoods))
+print(favFoods[-5])
+
+favFoods.append("Lucas")
+favFoods.insert(2, "17")
+print(favFoods)
+
+favFoods.remove("fried rice")
+print(favFoods)
+
+one = 1
+for x in range(0, 20):
+    print(one + x)
+
+for x in range(0, 20, 2):
+    print(one + x)
+
+animals = ["tiger", "lion", "panther"]
+
+for y in animals:
+    print(f"A {y} counts as a cat.")
+else:
+    print("All of these animals are dangerous!")
+
+guest_list = ["Einstein", "Aristotle", "Newton"]
+for z in guest_list:
+    print(f"{z} is invited to my dinner.")
+print("Aristotle can't make it.")
+guest_list.append("Obama")
+for z in guest_list:
+    print(f"{z} is invited to my dinner")
+    if z == "Aristotle":
+        continue
